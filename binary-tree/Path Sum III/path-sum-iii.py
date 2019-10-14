b2996f87d7cded013c260d09f12e9c06002c24b7
shardool <shardool_patel@hotmail.com>
2019-10-14 18:10:30 -0400

    add path sum III solution

--- binary-tree/Path Sum III/path-sum-iii.py
+++ binary-tree/Path Sum III/path-sum-iii.py
@@ -1,17 +1,3 @@
-[EASY] 
-# Path Sum III
-You are given a binary tree in which each node contains an integer value.
-
-Find the number of paths that sum to a given value.
-
-The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
-
-The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
-
-
-## Solution
-Maintain a stack of possible values that add up to the sum. When a new node is reached update all the paths and see if values reaches 0, meaning a path sums to the given *sum* value. Pop the root value and update the path when both trees are checked.
-```
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> int:
         self.valStack = []
@@ -34,5 +20,3 @@ class Solution:
         self.valStack.pop()
         for i in range(len(self.valStack)):
             self.valStack[i]+=root.val
-
-```
