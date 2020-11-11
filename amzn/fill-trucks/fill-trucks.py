cedba68ed73ddd81b6cb61cb4ee2606437a37659
shardool <shardool_patel@hotmail.com>
2020-11-10 22:58:37 -0500

    add amzn problems

---
+++ amzn/fill-trucks/fill-trucks.py
@@ -0,0 +1,74 @@
+"""
+    An Amazon Warehouse manager needs to create a shipment to fill a truck. All of the products in the warehouse are in boxes of 
+    the same size. Each product is packed in some number of units per box. Given the number of boxes the truck can hold, write an 
+    algorithm to determine the maximum number of units of any mix of products that can be shipped.
+
+    Input
+    The input to the function method consists of five arguments:
+    num, an integer representing number of products;
+    boxes, a list of integers representing the number of available boxes for products;
+    unitSize, an integer representing size of unitsPerBox;
+    unitsPerBox, a list of integers representing the number of units packed in each box;
+    truckSize, an integer representing the number of boxes the truck can carry.
+
+    Output
+    Return an integer representing the maximum units that can be carried by the truck.
+
+    Constraints
+    1 <= |boxes| <= 10^5
+    |boxes| == |unitsPerBox|
+    1 <= boxes[i] <= 10^7
+    1 <= i <= |boxes|
+    1 <= unitsParBox[i] <= 10^5
+    1 <= j <= |unitsPerBox|
+    1 <= truckSize <= 10 ^ 8
+
+    Note
+    [list name] denotes length of the list.
+
+    Example
+
+    Input:
+    num=3
+    boxes=[1,2,3]
+    unitSize=3
+    unitsPerBox= [3,2,1]
+    truckSize = 3
+
+    Output
+    7
+
+    Explanation:
+    Product 0: because boxes[0] = 1, we know there is 1 box in product 0. And because unitsPerBox[0] = 3, we know there is 1 box with 3 units in product 0.
+    Product 1: 2 boxes with 2 units each
+    Product 2: 3 boxes with 1 unit each
+    Final we have the packed products like a list : [3, 2, 2, 1, 1, 1]
+    The truckSize is 3, so we pick the top 3 from the above list, which is [3, 2, 2], and return the sum 7.
+    The maximum number of units that can be shipped = 3 + 2 + 2 = 7 units
+"""
+
+def fill_truck(boxes, units, truckSize):
+
+    # [3, 4, 6]
+    # [5, 3 ,2]
+    # truck size = 4 boxes
+
+    products = [(boxes[i], units[i]) for i in range(len(boxes))]
+    products.sort(reverse=True)
+    print(products)
+    units_loaded = 0
+    boxes_loaded = 0
+    i=0
+    while (boxes_loaded < truckSize and i < len(boxes)):
+        p_units, p_boxes = products[i]
+        boxes_to_load = min(p_boxes, truck_size - boxes_loaded) 
+        boxes_loaded += boxes_to_load
+        units_loaded += boxes_to_load * p_units
+        i+=1 
+    return units_loaded
+    #  3
+    # return max number of units you can fit in truck
+test_boxes = [5, 23, 63]
+test_units = [53, 23, 34]
+truck_size = 3
+print(fill_truck(test_boxes, test_units, truck_size))
--- arrays/player-rank/player-rank.py
+++ arrays/player-rank/player-rank.py
@@ -1,14 +1,31 @@
-def Solution():
-  def playerRankUp(self, scores, cutOff):
-    scores.sort(reversed=True)
-    curRank = 1
-    prevScore = -1
-    i=0
-    while curRank <= cutOff:
-      curScore = scores[i]
-      if (prevScore != curScore):
-        curRank+=1
-      i+=1
-    return ans
-    
+"""
+  Players playing a game where they have a cutoff rank to level up their character. 
+  Given scores of at the end of round how many players will be able to level up their 
+  character?
+
+  Players with equal scores will have equal ranks, but the player with
+  the next lowest score will be ranked based on the position within the list of all players'
+  scorres. 
+  for example, if there are four players, and three players tie for first place,
+  their ranks would be 1, 1, 1 and 4. also, no player with score of 0 can level up, no
+  matter their rank.
+
+  Determine count of players able to level up their rank
+
+"""
+
+def player_rank(scores, cutOffRank):
+  counts = [0 for i in range(101)]
+  for score in scores:
+    counts[score]+=1
+
+  rank = 0
+  for i in range(100, -1, -1):
+    rank+=counts[i]
+    if (cutOffRank <= rank):
+      break
+  
+  return rank
+
+print(player_rank([25,25,25,50,100], 2))
 
